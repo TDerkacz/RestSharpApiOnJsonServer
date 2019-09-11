26d58f7b63f0a63f50ea1b0003f00c68a41444a4
teder <t.der@wp.pl>
2019-09-11 23:55:27 +0200

    Response content as string by the GET method

--- RestSharpApiOnJsonServer/UnitTest1.cs
+++ RestSharpApiOnJsonServer/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
 
 namespace RestSharpApiOnJsonServer
 {
@@ -8,6 +9,15 @@ namespace RestSharpApiOnJsonServer
         [TestMethod]
         public void TestMethod1()
         {
+            var client = new RestClient("http://localhost:3000");
+
+            var request = new RestRequest("post/{postid}", Method.GET);
+            // refer to resource
+            request.AddUrlSegment("postid", 1);
+
+            var content = client.Execute(request).Content;
+            // response is a string
+
         }
     }
 }

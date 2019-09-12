0b46a262b3591f5b92a5d82d0d9efccdc4ae58b5
teder <t.der@wp.pl>
2019-09-12 17:24:40 +0200

    Moving Post model into Model folder

---
+++ RestSharpApiOnJsonServer/Model/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiExcercisingJsonServer.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string Language { get; set; }
+    }
+}

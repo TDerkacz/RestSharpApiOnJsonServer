aca12ebc40878f9ae57edeb277e0ee3a1c4cf119
Tadeusz Derkacz <44738745+TDerkacz@users.noreply.github.com>
2019-09-12 13:20:10 +0200

    Post call based on data model

--- RestSharpApiOnJsonServer/UnitTest1.cs
+++ RestSharpApiOnJsonServer/UnitTest1.cs
@@ -1,6 +1,7 @@
 using RestSharp;
 using RestSharp.Serialization.Json;
 using System.Collections.Generic;
+using ApiExcercisingJsonServer.Models;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using Assert = NUnit.Framework.Assert;
@@ -67,6 +68,28 @@ namespace RestSharpApiOnJsonServer
             var result = output["name"];
 
             Assert.That(result, Is.EqualTo("John"), "Name is different than expected");
+        }
+
+        [Test]
+        public void PostWithDataModel()
+        {
+            IRestClient client = new RestClient("http://localhost:3000");
+
+            RestRequest request = new RestRequest("post", Method.POST);
+
+            
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(new Post(){ Id = "4", Key = "CARS", Language = "EN_FI" });
+            request.AddUrlSegment("postid", 1);
+
+            IRestResponse response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+
+            var result = output["Key"];
+
+            Assert.That(result, Is.EqualTo("CARS"), "Key value is different than expected");
         }
     }
 }

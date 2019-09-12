5c70b78b263a8dec7a10b64f1709947b92606ce5
Tadeusz Derkacz <44738745+TDerkacz@users.noreply.github.com>
2019-09-12 09:26:56 +0200

    Instructions - remove MSTest and use NUnit assertions

--- RestSharpApiOnJsonServer/UnitTest1.cs
+++ RestSharpApiOnJsonServer/UnitTest1.cs
@@ -2,13 +2,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
 using RestSharp.Serialization.Json;
 using System.Collections.Generic;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
 
 namespace RestSharpApiOnJsonServer
 {
-    [TestClass]
+    // [TestClass] MSTest
+    [TestFixture] // NUnit
     public class UnitTestGetResponse
     {
-        [TestMethod]
+        // [TestMethod] MSTest
+        [Test] // NUnit
         public void TestMethodGetResponse()
         {
             IRestClient client = new RestClient("http://localhost:3000");
@@ -27,6 +31,22 @@ namespace RestSharpApiOnJsonServer
             var deserialize = new JsonDeserializer();
             var output = deserialize.Deserialize<Dictionary<string, string>>(response);
 
+            var result = output["key"];
+            Assert.That(result, Is.EqualTo("NEW"));
+
+
+            // ---> add package
+            // NUnit
+
+            // ---> remove packages
+
+            // Microsoft.VisualStudio.Test
+            // Microsoft.VisualStudio.TestTools.UnitTesting.Assert();
+
+            // MSTest.TestAdapter
+            // MSTest.TestFramework
+            // [TestClass] MSTest
+            // [TestMethod] MSTest
         }
     }
 }

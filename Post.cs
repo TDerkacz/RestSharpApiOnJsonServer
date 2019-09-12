e70a3e2b0ec451650ad16d5e92412ac5190f384e
Tadeusz Derkacz <44738745+TDerkacz@users.noreply.github.com>
2019-09-12 13:19:09 +0200

    Post model with structure

--- Post.cs
+++ Post.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace RestSharpApiOnJsonServer.Models
+namespace ApiExcercisingJsonServer.Models
 {
-    class Post
+    public class Post
     {
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string Language { get; set; }
     }
 }
